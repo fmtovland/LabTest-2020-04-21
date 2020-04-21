beef8842952076587e3e81ae29338032773d6e14
Liam McCormick <ljmc2000@gmail.com>
2020-04-21 10:29:17 +0100

    traffic light will now change color according to the behaviour specified in lab test doc

---
+++ Assets/TrafficLight.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLight : MonoBehaviour
+{
+	public enum Colour{GREEN,ORANGE,RED};
+	public Material[] materials;
+	public Colour colour;
+	
+	// Start is called before the first frame update
+	void Start()
+	{
+		SetColour((Colour)Random.Range(0,materials.Length));
+		StartCoroutine(ChangeColour());
+	}
+
+	IEnumerator ChangeColour()
+	{
+		while(true)
+		{
+			if(colour==Colour.ORANGE)
+			{
+				yield return new WaitForSeconds(4);
+			}
+			
+			else	//red and green have the same behaviour. if they need
+					//to specify different times an else if block for each
+					//should be created. alternatively the entire if else
+					//block could be converted to a switch statement
+			{
+				yield return new WaitForSeconds(Random.Range(5,10));
+			}
+			
+			SetColour((Colour)((int)(colour+1)%materials.Length));
+		}
+	}
+	
+	public void SetColour(Colour colour)
+	{
+		this.colour=colour;
+		GetComponent<Renderer>().material=materials[(int)colour];
+	}
+}
